4487c8d4613529e49251269b5c78b7f00986cbf7
DanniHills <danixahills@gmail.com>
2021-08-19 11:47:03 -0400

    header con angular material

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,7 +5,7 @@ import { ExponentialPipe } from './pipes/exponential/exponential.pipe';
 import { HighlightDirective } from './directives/highlight/highlight.directive';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-
+import {MaterialModule} from './../material/material.module'
 
 @NgModule({
   declarations: [
@@ -16,7 +16,8 @@ import { FooterComponent } from './components/footer/footer.component';
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule, 
+    MaterialModule
   ],
   exports:[
     HeaderComponent,
