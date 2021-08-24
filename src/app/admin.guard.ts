a556eac3c5e0e3cf6a258a5335c85257f9090d42
DanniHills <danixahills@gmail.com>
2021-08-24 01:45:42 -0400

    guard modificado(tap)

--- src/app/admin.guard.ts
+++ src/app/admin.guard.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree ,Router} from '@angular/router';
 import { Observable } from 'rxjs';
 import { map,tap } from 'rxjs/operators';
-import {AuthService} from './core/service/auth.service'
+import {AuthService} from './core/service/auth.service';
+
 @Injectable({
   providedIn: 'root'
 })
 export class AdminGuard implements CanActivate {
   constructor(
-    private authService :AuthService
+    private authService :AuthService,
+    private router : Router
   ){
 
   }
@@ -16,8 +18,13 @@ export class AdminGuard implements CanActivate {
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     return this.authService.hasUser().pipe(
-      tap(user=> console.log(user)),
-      map(user=> user === null  ? false : true  )
+      map(user=> user === null  ? false : true ),
+        tap(hasUser =>{
+          if(!hasUser)
+          {
+            this.router.navigate(['/auth/login'])
+          }
+        }),
     );
   }
   
